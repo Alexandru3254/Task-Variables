752484d12774122df9bc70aef87f8c12e0917c8e
Alexandru Globa <globa.alexandru02@gmail.com>
2024-09-28 14:44:24 +0300

    Commit Push

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+class Variables {
+    public static void main(String[] args) {
+
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabe = 'A';
+        boolean JavaIsAmazing = true;
+        float Pi = 3.14159F;
+        System.out.println("CurrentWorldPopulation = " + CurrentWorldPopulation);
+        System.out.println("CarsProducedThisYear = " + CarsProducedThisYear);
+        System.out.println("FirstLetterInAlphabe = " + FirstLetterInAlphabe);
+        System.out.println("JavaIsAmazing = " + JavaIsAmazing);
+        System.out.println("Pi = " + Pi);
+    }
+
+}
